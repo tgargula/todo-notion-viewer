96cf9adde2443f8d18c3d197b3f0ae2d810d50d3
tgargula <tomaszgargula1@gmail.com>
2022-06-10 00:51:03 +0200

    feat: add config tweaks

--- components/TaskGroupList.tsx
+++ components/TaskGroupList.tsx
@@ -1,22 +1,37 @@
 import { useCallback, useEffect, useState } from "react";
-import { FlatList, View, RefreshControl } from "react-native";
+import { FlatList, View, RefreshControl, Text, StyleSheet } from "react-native";
 import { fetchTasks } from "../actions/fetchTasks";
 import { renderTaskGroup, TaskGroupProps } from "./TaskGroup";
 
 export function TaskGroupList() {
   const [tasks, setTasks] = useState<TaskGroupProps[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     onRefresh();
   }, []);
 
   const onRefresh = useCallback(async () => {
+    setError(false);
     setRefreshing(true);
-    const fetchedTasks = await fetchTasks();
-    setTasks(fetchedTasks);
-    setRefreshing(false);
+    try {
+      const fetchedTasks = await fetchTasks();
+      setTasks(fetchedTasks);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setRefreshing(false);
+    }
   }, [refreshing]);
+
+  if (error) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={styles.error}>An error occured ❌</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={{ flex: 1 }}>
@@ -30,3 +45,11 @@ export function TaskGroupList() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  error: {
+    color: "#fff",
+    marginTop: 16,
+    alignSelf: "center",
+  },
+});
